2592e5401e11cf1262d5f587332002a85d8f5c04
rossatoe <124811784+rossatoe@users.noreply.github.com>
2024-09-09 17:09:31 +0000

    update

--- M2T1_rossatoe/hello.py
+++ M2T1_rossatoe/hello.py
@@ -5,5 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+
 def hello_world():
-    return "<p>Hello, world page!</p>"
+    return "<p>Hello, world ---- page!</p>"
+
+@app.route("/a")
+
+def hello_worldA():
+    return "<p>Hello, world --aaa-- page!</p>"
