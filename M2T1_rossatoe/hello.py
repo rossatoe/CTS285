1754efd84ed247091cfbae349277dd9bb2ec7a54
rossatoe <124811784+rossatoe@users.noreply.github.com>
2024-09-16 16:46:37 +0000

    added bootstrap

--- M2T1_rossatoe/hello.py
+++ M2T1_rossatoe/hello.py
@@ -4,11 +4,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config["DEBUG"] - True
+
 @app.route("/")
 
 def index():
-    name = "Jhon"
-    return render_template("main_page.html", name=name)
+    return render_template("main_page.html")
     
 
 @app.route("/action")
