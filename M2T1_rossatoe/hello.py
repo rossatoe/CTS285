bbeefcb768f0a8952320b0a001112ccf79d91b8c
rossatoe <124811784+rossatoe@users.noreply.github.com>
2024-09-16 17:03:46 +0000

    copy bootstrap html

--- M2T1_rossatoe/hello.py
+++ M2T1_rossatoe/hello.py
@@ -1,15 +1,19 @@
 # minimal flask app
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-app.config["DEBUG"] - True
+comments = []
 
-@app.route("/")
-
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
     
 
 @app.route("/action")
