31b6ae1272317c52b7da8c5fc5f859844d3f0baa
rossatoe <124811784+rossatoe@users.noreply.github.com>
2024-09-29 23:00:40 +0000

    read me file

---
+++ dataMan/README.md
@@ -0,0 +1,8 @@
+# Set Up the Environment
+- python -m venv venv
+- source venv/bin/activate
+# Installing and verifying Flask
+- pip install flask
+- python -m flask --version
+# Run spp.py Flask App
+- python app.py
--- dataMan/app.py
+++ dataMan/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, aaaaa World!"
+    return "Hello, bbb World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
