4071a15bd53be32b85ff601d9798215828cc96f3
rossatoe <124811784+rossatoe@users.noreply.github.com>
2024-09-30 00:08:18 +0000

    Step 5 - Display All Posts

--- dataMan/app.py
+++ dataMan/app.py
@@ -1,14 +1,20 @@
+import sqlite3
 from flask import Flask, render_template
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
-@app.route('/goodbye')
-def goodbye():
-    return "Goodbye, World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()
