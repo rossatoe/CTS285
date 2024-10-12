13e9545ecdabdd315965b9c22311527bb823f468
rossatoe <124811784+rossatoe@users.noreply.github.com>
2024-10-12 17:37:09 -0400

    Starting the actual dataman.py

--- dataMan/app.py
+++ dataMan/app.py
@@ -1,87 +1,28 @@
-import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
-from werkzeug.exceptions import abort
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
-
 
 @app.route('/')
-def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
+def home():
+    return render_template('index.html')
 
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
+@app.route('/answer_checker')
+def answer_checker():
+    return render_template('answer_checker.html')
 
-@app.route('/create', methods=('GET', 'POST'))
-def create():
+@app.route('/memory_bank', methods=['GET', 'POST'])
+def memory_bank():
     if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+        # Save the equations to a database or file (we'll implement this later)
+        pass
+    return render_template('memory_bank.html')
 
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
-    return render_template('create.html')
-
-@app.route('/<int:id>/edit', methods=('GET', 'POST'))
-def edit(id):
-    post = get_post(id)
-
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
     if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
-                         ' WHERE id = ?',
-                         (title, content, id))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
-
-    return render_template('edit.html', post=post)
-
-@app.route('/<int:id>/delete', methods=('POST',))
-def delete(id):
-    post = get_post(id)
-    conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
-    flash('"{}" was successfully deleted!'.format(post['title']))
-    return redirect(url_for('index'))
-
-
-
+        # Save the feedback to a database or file (we'll implement this later)
+        pass
+    return render_template('feedback.html')
 
 if __name__ == '__main__':
     app.run()
