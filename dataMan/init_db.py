01654e5bcada4eeda8c10a0fb9fbac6bb5edb1d4
rossatoe <124811784+rossatoe@users.noreply.github.com>
2024-10-12 17:43:20 -0400

    Update init_db.py

--- dataMan/init_db.py
+++ dataMan/init_db.py
@@ -1,20 +1,11 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
+def init_db():
+    conn = sqlite3.connect('database.db')
+    with open('schema.sql', 'r') as f:
+        conn.executescript(f.read())
+    conn.commit()
+    conn.close()
 
-
-with open('schema.sql') as f:
-    connection.executescript(f.read())
-
-cur = connection.cursor()
-
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
-            )
-
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
-            )
-
-connection.commit()
-connection.close()
+if __name__ == '__main__':
+    init_db()
